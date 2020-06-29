c3706e91c147cda197092c4632d974e11eba72cc
YuniceXiao <fishxy2@outlook.com>
2020-06-29 12:29:19 -0400

    add marker

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ mapboxgl.accessToken = "pk.eyJ1IjoibmduYXQiLCJhIjoiY2tjMG9rYTluMWxwdDMwcWR1MjV4N
 
 const map = new mapboxgl.Map({
   container: "map",
-  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  center: [-74.009, 40.705], // FullStack NY coordinates. Chicago: [-87.641, 41.895]
   zoom: 12, // starting zoom
-  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available
 });
+
+const markerDomEl = document.createElement("div");
+markerDomEl.style.width = "100px";
+markerDomEl.style.height = "100px";
+markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+new mapboxgl.Marker(markerDomEl).setLngLat([-74.009, 40.705]).addTo(map);
