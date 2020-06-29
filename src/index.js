7485f2dfcdbd8bf198e3052417f14097ef165fb4
natalie_ng <natalie.ng.14755@gmail.com>
2020-06-29 12:03:51 -0400

    Added map

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Hello world!")
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = "pk.eyJ1IjoibmduYXQiLCJhIjoiY2tjMG9rYTluMWxwdDMwcWR1MjV4N3hqYSJ9.IzcD6gCnp79swP5ClxsMQg";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
